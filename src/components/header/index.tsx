84ed80936775f57c3438a168faea627e4d104326
ww-side <daniil.metelia@gmail.com>
2025-10-16 16:44:10 +0300

    add container

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,25 +4,26 @@ import styles from './header.module.css'
 import { Link } from 'react-router-dom'
 
 const Header: FC = () => {
-
   return (
     <header className={styles.header}>
-      <div className={styles.links}>
-        <Link to="/shames" className={styles.link}>
-          Зашквари
-        </Link>
-        <Link to="/rating" className={styles.link}>
-          Особи
-        </Link>
-      </div>
-      <Link to="/">
-        <img src={logo} alt="logo" className={styles.logo} width={60} height={60} />
-      </Link>
-      <div className={styles.links}>
-          <Link to="/about-us" className={styles.link}>
-              Про нас
+      <section className={styles.layout}>
+        <div className={styles.links}>
+          <Link to="/shames" className={styles.link}>
+            Зашквари
           </Link>
-      </div>
+          <Link to="/rating" className={styles.link}>
+            Особи
+          </Link>
+        </div>
+        <Link to="/">
+          <img src={logo} alt="logo" className={styles.logo} width={60} height={60} />
+        </Link>
+        <div className={styles.links}>
+          <Link to="/about-us" className={styles.link}>
+            Про нас
+          </Link>
+        </div>
+      </section>
     </header>
   )
 }
