f3df1b1b1a960470091735f66ce0752591bde0bb
Andrii Lebedenko <126463380+LebedenkoAndrii@users.noreply.github.com>
2026-03-01 14:11:06 +0200

    Feature/change shame deputy photo (#50)
    * changed profile picture of deputy on shame card
    
    * add random deputy instead first deputy

--- src/components/shameCard/index.tsx
+++ src/components/shameCard/index.tsx
@@ -5,16 +5,28 @@ import { Icon } from '@iconify/react/dist/iconify.js';
 import { Link } from 'react-router-dom';
 
 const ShameCard: FC<ShameCardInfo> = shameCard => {
-  const imageUrl = shameCard.deputats[0]?.photo?.url || shameCard.deputats[0]?.photo?.url;
-  const fullname = shameCard.deputats[0].firstName + ' ' + shameCard.deputats[0].lastName;
+  const { deputats } = shameCard;
 
-  const deputiesCount = shameCard.deputats.length - 1;
+  if (!deputats || deputats.length === 0) return null;
+
+  const mayor = deputats.find(d => d.placeOfEmployment?.toLowerCase().includes('міський голова'));
+
+  const secretary = deputats.find(d => d.placeOfEmployment?.toLowerCase().includes('секретар'));
+
+  const deputyIndex = shameCard.documentId ? shameCard.documentId.length % deputats.length : 0;
+
+  const priorityDeputy = mayor || secretary || deputats[deputyIndex];
+
+  const imageUrl = priorityDeputy?.photo?.url || '';
+  const fullname = `${priorityDeputy?.firstName} ${priorityDeputy?.lastName}`;
+
+  const deputiesCount = deputats.length - 1;
 
   return (
     <Link to={`/details/${shameCard.documentId}`} className={styles.cardLink}>
       <div className={styles.shameCard}>
         <div className={styles.shameCardHead}>
-          <img src={imageUrl} alt="portret" className={styles.shameCardImage} />
+          {imageUrl && <img src={imageUrl} alt="portret" className={styles.shameCardImage} />}
           <div>
             <p className={styles.shameCardName}>{fullname}</p>
           </div>
